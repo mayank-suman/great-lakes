1ccfcd63eb7c0a909ac1b80babac03d42e814cc2
Mayank Suman <mayank@simpplr.com>
2021-04-10 14:57:28 +0530

    update layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,23 @@
 import React from "react";
-// import Button from "@material-ui/core/Button";
-// import Venue from "./Venue.jsx";
-import Lakes from "./lakes";
-
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
+import { ThemeProvider } from "@material-ui/core/styles";
 
+import theme from "../theme";
+import Lakes from "./lakes";
+// import Venue from "./Venue.jsx";
 class App extends React.Component {
   render() {
     return (
       <>
-        <CssBaseline />
-        <Container maxWidth="sm">
-          <Typography
-            component="div"
-            style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
-          >
-            {/* <Button variant="contained">this is a material UI button</Button> */}
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Container maxWidth="sm">
             {/* <Venue /> */}
             <Lakes />
-          </Typography>
-        </Container>
+          </Container>
+        </ThemeProvider>
       </>
     );
   }
