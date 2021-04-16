9ff986de36af6893a2df0107d21be4cb3e99e5b8
Mayank Suman <mayank@simpplr.com>
2021-04-16 14:06:04 +0530

    refactor to image lake cover comp

---
+++ src/components/lakes/lakeCard/cover.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import CardMedia from "@material-ui/core/CardMedia";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { getPhoto } from "../../../api/googleMaps";
+
+const useStyles = makeStyles((theme) => ({
+  loader: {
+    margin: theme.spacing(2),
+  },
+}));
+
+function cover({ photos }) {
+  const classes = useStyles();
+  const [getPhotoUrl, setPhotoUrl] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const res = await getPhoto({
+        referenceId: photos && photos[0] && photos[0].photo_reference,
+        maxwidth: 250,
+      });
+
+      setPhotoUrl(res.url);
+    })();
+  }, []);
+
+  return (
+    <>
+      {getPhotoUrl ? (
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="150"
+          image={getPhotoUrl}
+          title="Contemplative Reptile"
+        />
+      ) : (
+        <Grid container direction="row" justify="center" alignItems="center">
+          <CircularProgress color="inherit" className={classes.loader} />
+        </Grid>
+      )}
+    </>
+  );
+}
+
+export default cover;
--- src/components/lakes/lakeCard/index.jsx
+++ src/components/lakes/lakeCard/index.jsx
@@ -5,40 +5,24 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import StarIcon from "@material-ui/icons/Star";
 import IconButton from "@material-ui/core/IconButton";
 import ExploreIcon from "@material-ui/icons/Explore";
 
-import { getPhoto, getMapLinkUrl } from "../../api/googleMaps";
+import Cover from "./cover";
+import { getMapLinkUrl } from "../../../api/googleMaps";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     // maxWidth: 345,
     margin: theme.spacing(1),
   },
-  loader: {
-    margin: theme.spacing(2),
-  },
 }));
 
 export default function LakeCard({ itemProps }) {
   const classes = useStyles();
   const { name, photos } = itemProps;
-  const [getPhotoUrl, setPhotoUrl] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      const res = await getPhoto({
-        referenceId: photos && photos[0] && photos[0].photo_reference,
-        maxwidth: 250,
-      });
-
-      setPhotoUrl(res.url);
-    })();
-  }, []);
 
   // TODO: create photos gallery modal
   return (
@@ -46,19 +30,7 @@ export default function LakeCard({ itemProps }) {
       <CardActionArea>
         {/* TODO: add skeleton */}
         {/* TODO: use responsive images */}
-        {getPhotoUrl ? (
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="150"
-            image={getPhotoUrl}
-            title="Contemplative Reptile"
-          />
-        ) : (
-          <Grid container direction="row" justify="center" alignItems="center">
-            <CircularProgress color="inherit" className={classes.loader} />
-          </Grid>
-        )}
+        <Cover photos={photos} />
         <CardContent>
           <Typography gutterBottom variant="h6" component="h2">
             {name}
