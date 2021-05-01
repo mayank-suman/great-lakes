9081379c3d83f28c00a6225112caa58f6e5ea3fe
Mayank Suman <mayank@simpplr.com>
2021-05-02 00:34:14 +0530

    add google api env var

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -7,6 +7,7 @@ const { GOOGLE_MAPS_API_BASE_URL } = require("./constants");
 module.exports = merge(common, {
   mode: "production",
   plugins: [
+    new webpack.EnvironmentPlugin(["GOOGLE_MAPS_PLACES_API_KEY"]),
     new webpack.DefinePlugin({
       GOOGLE_MAPS_API_BASE_URL: JSON.stringify(GOOGLE_MAPS_API_BASE_URL),
     }),
