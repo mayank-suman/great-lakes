bacdc66e596582dad3828c1e48b279bc0986ee86
Mayank Suman <mayank@simpplr.com>
2021-05-02 01:03:45 +0530

    add netlify plugin to set header

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const webpack = require("webpack");
+const { NetlifyPlugin } = require("netlify-webpack-plugin");
 
 const common = require("./webpack.common.js");
 const { GOOGLE_MAPS_API_BASE_URL } = require("./constants");
@@ -10,6 +11,13 @@ module.exports = merge(common, {
     new webpack.EnvironmentPlugin(["GOOGLE_MAPS_PLACES_API_KEY"]),
     new webpack.DefinePlugin({
       GOOGLE_MAPS_API_BASE_URL: JSON.stringify(GOOGLE_MAPS_API_BASE_URL),
+    }),
+    new NetlifyPlugin({
+      headers: {
+        "index.html": {
+          "Access-Control-Allow-Origin": "https://greatlakes.mayanksuman.dev",
+        },
+      },
     }),
   ],
 });
