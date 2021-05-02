879f0eedb01f3e9d01d828b9bf57ec477fb178e0
Mayank Suman <mayank@simpplr.com>
2021-05-02 22:32:49 +0530

    add broken image icon

--- src/components/lakes/lakeCard/cover.jsx
+++ src/components/lakes/lakeCard/cover.jsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from "react";
 import CardMedia from "@material-ui/core/CardMedia";
 import { makeStyles } from "@material-ui/core/styles";
 import Skeleton from "@material-ui/lab/Skeleton";
+import BrokenImageIcon from "@material-ui/icons/BrokenImage";
 
 import { getPhoto } from "../../../api/googleMaps";
+import { Grid } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  loader: {
-    margin: theme.spacing(2),
-  },
   root: {
     height: 200,
     [theme.breakpoints.up("md")]: {
@@ -20,25 +19,37 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+let isFirstLoad = true;
+
 function cover({ photos }) {
   const classes = useStyles();
   const [getPhotoUrl, setPhotoUrl] = useState("");
+  const [isLoading, setLoading] = React.useState(false);
 
   useEffect(() => {
+    isFirstLoad = false;
     (async () => {
+      setLoading(true);
       const res = await getPhoto({
         referenceId: photos && photos[0]?.photo_reference,
         maxwidth: 600,
       });
 
+      setLoading(false);
       setPhotoUrl(res.url);
     })();
+
+    return () => {
+      isFirstLoad = true;
+    };
   }, []);
 
   //  TODO: use responsive images
   return (
     <>
-      {getPhotoUrl ? (
+      {isLoading ? (
+        <Skeleton variant="rect" className={classes.root} />
+      ) : getPhotoUrl ? (
         <CardMedia
           component="img"
           alt="Contemplative Reptile"
@@ -47,7 +58,15 @@ function cover({ photos }) {
           className={classes.root}
         />
       ) : (
-        <Skeleton variant="rect" className={classes.root} />
+        <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="center"
+          className={classes.root}
+        >
+          {!isFirstLoad && <BrokenImageIcon />}
+        </Grid>
       )}
     </>
   );
