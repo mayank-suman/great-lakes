d4dc214d61b839a0bb438d9bdf055d789f5c706b
Mayank Suman <mayank@simpplr.com>
2021-05-02 12:22:27 +0530

    create proxy for netlify server

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,21 +3,21 @@ const webpack = require("webpack");
 const { NetlifyPlugin } = require("netlify-webpack-plugin");
 
 const common = require("./webpack.common.js");
-const { GOOGLE_MAPS_API_BASE_URL } = require("./constants");
+const { GOOGLE_MAPS_API_BASE_URL, PROXY_BASE_PATH } = require("./constants");
 
 module.exports = merge(common, {
   mode: "production",
   plugins: [
     new webpack.EnvironmentPlugin(["GOOGLE_MAPS_PLACES_API_KEY"]),
-    new webpack.DefinePlugin({
-      GOOGLE_MAPS_API_BASE_URL: JSON.stringify(GOOGLE_MAPS_API_BASE_URL),
-    }),
     new NetlifyPlugin({
-      headers: {
-        "index.html": {
-          "Access-Control-Allow-Origin": "*",
+      redirects: [
+        {
+          from: `${PROXY_BASE_PATH}/**`,
+          to: `${GOOGLE_MAPS_API_BASE_URL}/:splat`,
+          status: 200,
+          force: true,
         },
-      },
+      ],
     }),
   ],
 });
