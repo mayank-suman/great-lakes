e6a5cc5557b159fddcfa5e0199f387390b12a25a
Mayank Suman <mayank@simpplr.com>
2021-05-05 12:24:47 +0530

    add gallery button

--- src/components/lakes/lakeCard/item.jsx
+++ src/components/lakes/lakeCard/item.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import StarIcon from "@material-ui/icons/Star";
-import IconButton from "@material-ui/core/IconButton";
 import ExploreIcon from "@material-ui/icons/Explore";
+import Button from "@material-ui/core/Button";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 
 import Cover from "./cover.jsx";
 import { getMapLinkUrl } from "api/googleMaps";
 import LakesGalleryModal from "components/lakes/lakesGalleryModal.jsx";
 import useModal from "hooks/useModal";
-import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     // maxWidth: 345,
+    margin: theme.spacing(1),
+  },
+  button: {
     margin: theme.spacing(1),
   },
 }));
@@ -37,43 +39,52 @@ export default function LakeItem({ itemProps: place }) {
 
   function handleCardClick(e) {
     e.preventDefault;
-    console.log("click");
     handleOpen();
   }
 
   // TODO: create photos gallery modal
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <Modal />
-        <Cover photos={photos} />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {vicinity}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="div">
-            <Grid
-              container
-              direction="row"
-              justify="flex-start"
-              alignItems="center"
-            >
-              {rating}
-              <StarIcon fontSize="small" />
-            </Grid>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Modal />
+      <Cover photos={photos} />
+      <CardContent>
+        <Typography gutterBottom variant="h6" component="h2">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {vicinity}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="div">
+          <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="center"
+          >
+            {rating}
+            <StarIcon fontSize="small" />
+          </Grid>
+        </Typography>
+      </CardContent>
       <CardActions>
-        <IconButton aria-label="explore">
-          <a href={getMapLinkUrl(place)} target="blank">
-            <ExploreIcon />
-          </a>
-        </IconButton>
-        <Button onClick={handleCardClick}>Show more</Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          startIcon={<ExploreIcon />}
+          onClick={() => window.open(getMapLinkUrl(place))}
+        >
+          Explore
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          className={classes.button}
+          startIcon={<PhotoLibraryIcon />}
+          onClick={handleCardClick}
+        >
+          gallery
+        </Button>
       </CardActions>
     </Card>
   );
