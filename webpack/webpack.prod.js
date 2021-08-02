2077ec3e90b0fad1aedbcdde50c3a013b2076876
Mayank Suman <mayank.suman@simpplr.com>
2021-08-02 17:01:03 +0530

    fix google branding image not showing on prod

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const webpack = require("webpack");
 const { NetlifyPlugin } = require("netlify-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const common = require("./webpack.common.js");
 const { GOOGLE_MAPS_API_BASE_URL, PROXY_BASE_PATH } = require("./constants");
@@ -8,7 +9,7 @@ const { GOOGLE_MAPS_API_BASE_URL, PROXY_BASE_PATH } = require("./constants");
 module.exports = merge(common, {
   mode: "production",
   plugins: [
-    new webpack.EnvironmentPlugin(["GOOGLE_MAPS_PLACES_API_KEY"]),
+    new webpack.EnvironmentPlugin({ GOOGLE_MAPS_PLACES_API_KEY: "" }),
     new NetlifyPlugin({
       redirects: [
         {
@@ -16,6 +17,15 @@ module.exports = merge(common, {
           to: `${GOOGLE_MAPS_API_BASE_URL}/:splat`,
           status: 200,
           force: true, // needed for url rewriting
+        },
+      ],
+    }),
+    // This is done copy all files from public to build folder except html files
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "public",
         },
       ],
     }),
